21bb5545b9b492327afdec76bc2ace3cce900068
Parna324 <parnaghosh628@gmail.com>
2024-09-17 19:06:06 +0530

    add header link

---
+++ src/components/headerlink.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function headerlink() {
+  return (
+    <div className='flex'>
+      <ul>
+        <a href="">link1</a>
+        <a href="">link2</a>
+      </ul>
+    </div>
+  )
+}
+
+export default headerlink
