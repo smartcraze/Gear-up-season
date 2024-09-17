da28b81029b6d876061b24344c1b11c6a4f9c030
smartcraze <smartcraze354@gmail.com>
2024-09-17 11:48:38 +0530

    added login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import Login from '@/components/Login'
+import React from 'react'
+
+function page() {
+  return (
+    <Login/>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Login from "@/components/Login";
 
 
 export default function Home() {
   return (<>
     {/* <h1>sih</h1> */}
-    <Login/>
+    <h1>sih</h1>
   </>
   );
 }
